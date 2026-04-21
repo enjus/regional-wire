0b4dcb61c24db064d9e1a06d384f6285722bef07
enjus <elliot.njus@gmail.com>
2026-04-21 07:46:27 -0700

    Update member-orgs.ts
    New dummy data

--- lib/member-orgs.ts
+++ lib/member-orgs.ts
@@ -8,16 +8,16 @@ export type MemberOrg = {
 }
 
 export const MEMBER_ORGS: MemberOrg[] = [
-  { initials: 'NH', name: 'Northfield Herald',     location: 'Daily newspaper',      color: '#2D5A8B' },
-  { initials: 'DG', name: 'Daily Gazette',          location: 'Daily newspaper',      color: '#8B3A3A' },
-  { initials: 'RV', name: 'River Valley Report',    location: 'Nonprofit newsroom',   color: '#4A7C59' },
-  { initials: 'WC', name: 'Westchester Courier',    location: 'Weekly newspaper',     color: '#8B5E3C' },
-  { initials: 'SL', name: 'Star-Ledger',            location: 'Regional newspaper',   color: '#5A4A8B' },
-  { initials: 'BR', name: 'Blue Ridge Review',      location: 'Nonprofit newsroom',   color: '#3A5A8B' },
-  { initials: 'MT', name: 'Mountain Tribune',       location: 'Weekly newspaper',     color: '#8B7A3A' },
-  { initials: 'CP', name: 'Capital Press',          location: 'Daily newspaper',      color: '#6B3A8B' },
-  { initials: 'EJ', name: 'Eastern Journal',        location: 'Digital outlet',       color: '#3A8B6B' },
-  { initials: 'NR', name: 'Northern Record',        location: 'Weekly newspaper',     color: '#8B3A6B' },
-  { initials: 'LW', name: 'Lakewood Wire',          location: 'Nonprofit newsroom',   color: '#3A7A8B' },
-  { initials: 'PH', name: 'Prairie Herald',         location: 'Daily newspaper',      color: '#6B8B3A' },
+  { initials: 'H', 	name: 'The Herald',     		location: 'Northfield',		color: '#2D5A8B',	url:https://google.com },
+  { initials: 'DG', name: 'Daily Gazette',          location: 'Angel Grove',	color: '#8B3A3A' },
+  { initials: 'RV', name: 'River Valley Report',    location: 'Millersberg',	color: '#4A7C59' },
+  { initials: 'WC', name: 'Westchester Courier',    location: 'Westchester',	color: '#8B5E3C' },
+  { initials: 'SL', name: 'Star-Leader',            location: 'Jefferson',		color: '#5A4A8B' },
+  { initials: 'BR', name: 'Blue Ridge Review',      location: 'Graphite',		color: '#3A5A8B' },
+  { initials: 'MT', name: 'Mountain Tribune',       location: 'Blueston',		color: '#8B7A3A' },
+  { initials: 'CP', name: 'Capital Press',          location: 'Green Graze',	color: '#6B3A8B' },
+  { initials: 'EJ', name: 'Eastern Journal',        location: 'Fuschia',		color: '#3A8B6B' },
+  { initials: 'NR', name: 'Northern Record',        location: 'Billingston',	color: '#8B3A6B' },
+  { initials: 'LW', name: 'Lakewood Wire',          location: 'Stefans',		color: '#3A7A8B' },
+  { initials: 'PH', name: 'Prairie Herald',         location: 'Grove',			color: '#6B8B3A' },
 ]
